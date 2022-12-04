b80b216cd724bcc43e089d9a239296f62bf6a5a3
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2022-12-04 01:16:37 -0800

    Initial Commit

---
+++ gamerzbase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gamerzbase
+//
+//  Created by Kenneth Dubroff on 12/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gamerzbase/gamerzbaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  gamerzbaseApp.swift
+//  gamerzbase
+//
+//  Created by Kenneth Dubroff on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct gamerzbaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
